7eafafd3e055343a9f4b616ebaf031cb0a8ac9e4
daodo <nhokmoon300692@gmail.com>
2016-10-11 23:42:48 +0700

    nBIt

---
+++ tryBit/tryBit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryBit
+{
+    class Program
+    {
+        static private double[] A;
+        static private int n;
+        static void trynBit(int i)
+        {
+            double[] A = new double[4];
+            for(int j=0;j<=1;j++)
+            {
+                A[i] == j;
+                if (i == n - 1)
+                    Console.WriteLine(A[i]);
+                else trynBit(i + 1);
+            }
+            
+        }
+                
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            trynBit(0);
+        }
+    }
+}
